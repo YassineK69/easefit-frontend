c9241ea9a5eca44811f9d01dc10356308f7e904a
Dal ferro Christian <christiandalferro@gmail.com>
2025-08-05 11:33:36 +0200

    avant pull

--- components/birthday.js
+++ components/birthday.js
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, Modal, Platform, Button } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Modal,
+  Platform,
+  Button,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useEffect } from "react";
 
 const DatePickerWithModal = (props) => {
   const [date, setDate] = useState(new Date());
@@ -8,10 +17,12 @@ const DatePickerWithModal = (props) => {
 
   const onChange = (event, selectedDate) => {
     if (selectedDate) setDate(selectedDate);
-    if (Platform.OS === 'android') setShowModal(false); // Android ferme automatiquement la modale
+    if (Platform.OS === "android") setShowModal(false); // Android ferme automatiquement la modale
   };
 
- props.selectBirthday(date);
+  useEffect(() => {
+    props.selectBirthday(date);
+  }, [date]);
 
   const confirmIOSDate = () => setShowModal(false); // pour valider manuellement sur iOS
 
@@ -27,22 +38,35 @@ const DatePickerWithModal = (props) => {
             borderWidth: 1,
             padding: 10,
             borderRadius: 5,
-            borderColor: '#ccc',
+            borderColor: "#ccc",
           }}
         />
       </TouchableOpacity>
 
       <Modal visible={showModal} transparent animationType="slide">
-        <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: '#00000077' }}>
-          <View style={{ backgroundColor: 'white', padding: 16, borderTopLeftRadius: 20, borderTopRightRadius: 20 }}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+            backgroundColor: "#00000077",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 16,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          >
             <DateTimePicker
               value={date}
               mode="date"
               display="spinner"
               onChange={onChange}
-              style={{ backgroundColor: 'white' }}
+              style={{ backgroundColor: "white" }}
             />
-            {Platform.OS === 'ios' && (
+            {Platform.OS === "ios" && (
               <Button title="Valider" onPress={confirmIOSDate} />
             )}
           </View>
