d8e88c5f0a9e7b64eab65d856d031047b708e7eb
YassineK69 <kninech.yassine@hotmail.fr>
2025-08-14 15:12:53 +0200

    modification tab bar pour profil

--- components/CustomTabBarBackground.js
+++ components/CustomTabBarBackground.js
@@ -1,16 +1,23 @@
-
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient'; 
+import { StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BlurView } from 'expo-blur';
 
 const CustomTabBarBackground = () => {
   return (
-    <LinearGradient
-      colors={['#5e2a84', '#da341b']}
-      start={{ x: 0.5, y: 0 }}
-      end={{ x: 0.5, y: 1 }}
-      style={StyleSheet.absoluteFill}
-    />
+    <View style={StyleSheet.absoluteFill}>
+      <LinearGradient
+        colors={['#5e2a84', '#da341b']}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        style={StyleSheet.absoluteFill}
+      />
+      <BlurView
+        intensity={20} // Ajuste la force du flou
+        tint="dark"  // Peut être 'light', 'dark', ou 'default'
+        style={StyleSheet.absoluteFill}
+      />
+    </View>
   );
 };
 
