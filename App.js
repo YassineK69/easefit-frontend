3d8bc612bf0a40fb0d396f54670bccdd795693fa
Fanny <fannyboyer1d@gmail.com>
2025-08-15 15:54:06 +0200

    ajout app.js

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { Provider } from "react-redux";
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();
+
 import { persistStore, persistReducer } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
